5c4b93701e119fe3023266835ef0ab633b823d12
EliteAngelus <Angeluselite@gmail.com>
2018-08-15 22:21:53 -0400

    Added button and function to js file

---
+++ script.js
@@ -0,0 +1,29 @@
+// Make connection to database
+
+
+
+// Take in user input and populate the correct field
+
+function orderBurger() {
+
+    const userOrder = document.getElementById('order').value
+
+    alert(userOrder);
+
+    console.log(userOrder);
+
+    event.preventDefault();
+}
+
+// Handle devour function  
+
+function devour() {
+
+}
+
+// Handle "try again" function
+
+function tryAgain() {
+
+
+}
